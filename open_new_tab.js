1c4b386e74184e487917615c9501430f2cb07e61
leyle <leyle@leyle.com>
2020-01-16 16:59:38 +0800

    add open new table js

---
+++ open_new_tab.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name     OpenNewTab
+// @author   leyle@leyle.com
+// @include  https://www.uump4.net/*
+// @require  https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @grant    unsafeWindow
+// @run-at   document-end
+// @charset  UTF-8
+// ==/UserScript==
+
+var as = document.getElementsByTagName("a");
+
+for(var a of as) {
+    var href = a.href
+    if(href.includes("thread")) {
+        a.setAttribute("target", "_blank")
+    }
+}
